94e8c9078e3586c5975165bd907ca5b556edd476
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-05 02:20:44 +0530

    [Praveen] Add: Ensured the databse connectiona and get all the records from the table.

---
+++ AddressBookServices/AddressBookServices/Program.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Praveen Kumar Upadhyay"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+
+namespace AddressBookServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Welcome to the Address Book Data Retrieval Program");
+            Console.WriteLine("==================================================");
+            AddressBookRepository repository = new AddressBookRepository();
+            repository.EnsureDataBaseConnection();
+            Console.ReadKey();
+            /// UC1 -- Getting all the records from the address book table
+            repository.GetAllRecords();
+        }
+    }
+}
